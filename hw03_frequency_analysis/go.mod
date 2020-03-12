59ba83f71b9ae6a487f047a7949655bdaf84487e
Igor Nikitin <imneek@gmail.com>
2020-03-12 16:41:48 +0300

    HW03 is completed

--- hw03_frequency_analysis/go.mod
+++ hw03_frequency_analysis/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw03_frequency_analysis
+module github.com/neek38/otuslabs/hw03_frequency_analysis
 
 go 1.14
 
--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,52 @@
 package hw03_frequency_analysis //nolint:golint,stylecheck
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
 
-func Top10(_ string) []string {
-	// Place your code here
-	return nil
+type KeyValue struct {
+	Key   string
+	Value int
+}
+
+func Top10(inputtedString string) []string {
+	regexToSplit := regexp.MustCompile(`[0-9\n\t.,!?;: «»()—\"']+`)
+	onlySeparators := regexp.MustCompile(`^[0-9\n\t.,!?;:\- «»()—\"']+$`)
+	wordsMap := make(map[string]int)
+	var result []string
+
+	//var keyValueSlice []KeyValue
+	keyValueSlice := []KeyValue{}
+	if inputtedString == "" {
+		return result
+	}
+
+	split := regexToSplit.Split(inputtedString, -1)
+
+	for i := range split {
+		if !onlySeparators.MatchString(split[i]) {
+			loweredString := strings.ToLower(split[i])
+			_, ok := wordsMap[loweredString]
+			if ok {
+				wordsMap[loweredString]++
+			} else {
+				wordsMap[loweredString] = 1
+			}
+		}
+	}
+
+	for k, v := range wordsMap {
+		keyValueSlice = append(keyValueSlice, KeyValue{k, v})
+	}
+
+	sort.Slice(keyValueSlice, func(i, j int) bool {
+		return keyValueSlice[i].Value > keyValueSlice[j].Value
+	})
+
+	for i := 0; i < 10; i++ {
+		result = append(result, keyValueSlice[i].Key)
+	}
+
+	return result
 }
